976f7c02ffa97683a513f4a940eeb1196520de07
Tkachuk Ilya <tim@MacBook-Pro-Tkachuk.local>
2017-08-22 00:46:14 +0300

    concurrency vs parallelism

---
+++ examples/js-server.js
@@ -0,0 +1,13 @@
+var static = require('node-static');
+var file = new static.Server('./');
+
+module.exports = function () {
+  require('http').createServer(function (request, response) {
+      request.addListener('end', function () {
+          //
+          // Serve files!
+          //
+          file.serve(request, response);
+      }).resume();
+  }).listen(8080);
+}
