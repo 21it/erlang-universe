3fa52be0644c6d12107d4e8ba684cc32985be4b2
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2017-10-31 12:51:27 +0200

    add ExUnit and Coveralls

--- examples/bad_math/mix.exs
+++ examples/bad_math/mix.exs
@@ -7,7 +7,9 @@ defmodule BadMath.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -22,6 +24,7 @@ defmodule BadMath.Mixfile do
   defp deps do
     [
       {:dialyxir, "~> 0.5", only: [:dev, :test]},
+      {:excoveralls, "~> 0.7", only: :test},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
